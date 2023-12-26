f5368a9b230698cc7730ee24765c9282723f3765
ibreath-tech <ibreath.io@gmail.com>
2023-12-26 22:53:50 +0100

    config: change domain name

--- data/siteMetadata.ts
+++ data/siteMetadata.ts
@@ -7,13 +7,13 @@ export const siteMetadata = {
   metaRobots: 'index, follow',
   language: 'en-US',
   theme: 'system',
-  siteUrl: `https://herb-classifier.vercel.app`,
+  siteUrl: `https://herbify-app.vercel.app`,
 
   // Open Graph tags
-  ogImage: `https://herb-classifier.vercel.app/og-2-1.png`,
+  ogImage: `https://herbify-app.vercel.app/og-2-1.png`,
 
   // Twitter tags
-  twImage: `https://herb-classifier.vercel.app/og-2-1.png`,
+  twImage: `https://herbify-app.vercel.app/og-2-1.png`,
   locale: 'en-US',
   githubUrl: 'https://github.com/miliariadnane/herb-classifier-api',
 };
